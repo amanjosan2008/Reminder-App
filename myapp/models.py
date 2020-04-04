65473ba637c2a78d5e533d3a2a19043763ea118e
Amandeep Singh <amanjosan2008@gmail.com>
2020-04-04 17:21:30 +0530

    Time added, Last SMS, Success, Error features added

--- myapp/models.py
+++ myapp/models.py
@@ -3,7 +3,12 @@ from django.db import models
 class user(models.Model):
     name = models.CharField(max_length = 40)
     city = models.CharField(max_length = 40, default = 'Kapurthala')
-    phone = models.CharField(max_length = 10, unique = True)
+    phone = models.IntegerField(unique = True)
+    added_time = models.DateTimeField(auto_now_add=True)
+    #last_sms = models.DateTimeField(auto_now=True)
+    last_sms = models.DateTimeField(blank=True, null=True)
+    success = models.IntegerField(default=0, blank=True)
+    error = models.IntegerField(default=0, blank=True)
 
     def __str__(self):
         return self.name
--- myapp/views.py
+++ myapp/views.py
@@ -1,16 +1,17 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect
 from myapp.forms import userForm, messageForm
-from myapp.models import user,message_format
+from myapp.models import user, message_format
 from django.db import utils
 from twilio.base.exceptions import TwilioRestException
+import datetime
 
-
+# Home Page feature
 def home(request):
     users_list = user.objects.all()
     return render(request,'home.html',{'user_list':users_list})
 
-
+# Add User feature
 def add(request):
     if request.method == "POST":
         form = userForm(request.POST)
@@ -26,17 +27,16 @@ def add(request):
         form = userForm()
     return render(request,'add.html',{'form':form})
 
-
+# Delete User feature
 def destroy(request,id):
     user_list = user.objects.get(id=id)
     user_list.delete()
     return redirect("/home")
 
-
+# Edit User feature
 def edit(request, id):
     user_list = user.objects.get(id=id)
     return render(request,'edit.html', {'user':user_list})
-
 
 def edit_save(request, id):
     user_list = user.objects.get(id=id)
@@ -51,7 +51,7 @@ def edit_save(request, id):
         return HttpResponse("Invalid Data:" + str(form.errors.as_data()))
     return render(request, 'edit.html', {'user': user_list})
 
-
+# Send SMS feature
 def sendsms(request, id):
     #if request.method == 'POST' and 'send_a_sms' in request.POST:
         User = user.objects.get(id=id)
@@ -60,15 +60,31 @@ def sendsms(request, id):
         from send_sms import sms
         try:
             sms(name,phone)
+            success_inc(id)
         except TwilioRestException as e:
+            error_inc(id)
             return HttpResponse("TwilioRestException" + str(e))
         except Exception as ex:
+            error_inc(id)
             return HttpResponse("Exception" + str(ex))
         return redirect("/home")
     #else:
         #return HttpResponse("Wrong submit request")
 
+def success_inc(id):
+    record = user.objects.get(id=id)
+    value = record.success + 1
+    record.success = value
+    record.last_sms = datetime.datetime.now()
+    record.save()
 
+def error_inc(id):
+    record = user.objects.get(id=id)
+    value = record.error + 1
+    record.error = value
+    record.save()
+
+# Message Format feature
 def message(request):
     #if request.method == 'POST' and 'message_button' in request.POST:
     Message = message_format.objects.latest('id')
